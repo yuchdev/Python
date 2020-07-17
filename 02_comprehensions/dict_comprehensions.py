48ea919486aacbf449967754ef8e24690136858c
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2020-07-17 19:35:09 +0700

    compr

--- 02_comprehensions/dict_comprehensions.py
+++ 02_comprehensions/dict_comprehensions.py
@@ -1,17 +1,28 @@
 import os
-import stat
+import sys
 import glob
 
 
-def show_dict_comprehensions():
+def create():
     # A dictionary comprehension is like a list comprehension, but it constructs a dictionary instead of a list
     metadata_dict = {f: os.stat(f) for f in glob.glob('*.py')}
-    print(metadata_dict)
+    print("{{f: os.stat(f) for f in glob.glob('*.py')}}".format(metadata_dict))
 
     # Here’s a trick with dictionary comprehensions that might be useful someday:
     # swapping the keys and values of a dictionary (value also must be hashable)
     a_dict = {'a': 1, 'b': 2, 'c': 3}
+    print("{{'a': 1, 'b': 2, 'c': 3}}={}".format(a_dict))
     rev_dict = {value: key for key, value in a_dict.items()}
-    print(a_dict)
-    print(rev_dict)
+    print("{{value: key for key, value in a_dict.items()}}={}".format(rev_dict))
 
+
+if __name__ == '__main__':
+    """
+    Choose one of examples
+    """
+    create()
+    function = sys.argv[1]
+    try:
+        locals()[function]()
+    except KeyError as _:
+        print("Choose one of functions to call: create")
--- 02_comprehensions/list_comprehensions.py
+++ 02_comprehensions/list_comprehensions.py
@@ -1,23 +1,69 @@
 import os
-import stat
+import sys
 import glob
 
 
-def show_list_comprehensions():
+def create():
+
     # A list comprehension provides a compact way of mapping a list into another list by applying a function
     a_list = [1, 2, 3, 4]
-    print([elem*2 for elem in a_list])
+    print("a_list={}".format(a_list))
+    a_list2 = [elem * 2 for elem in a_list]
+    print("[elem*2 for elem in a_list]={0}".format(a_list2))
 
+    # another example
+    names = ['Elwood', 'Jake']
+    print("names={}".format(names))
+    names2 = [name.lower() for name in names]
+    print("[name.lower() for name in names]={}".format(names2))
+
+
+def files():
     # it does not change the original list
-
     files_in_dir = [os.path.realpath(f) for f in glob.glob('*.py')]
-    print(files_in_dir)
+    print("[os.path.realpath(f) for f in glob.glob('*.py')]={}".format(files_in_dir))
 
     # List comprehensions can also filter items
     big_in_dir = [os.path.realpath(f) for f in glob.glob('*.py') if os.stat(f).st_size > 600]
-    print(big_in_dir)
+    print("[os.path.realpath(f) for f in glob.glob('*.py') if os.stat(f).st_size > 600]={}".format(big_in_dir))
 
     # there’s no limit to how complex a list comprehension can be
     sizes = [(os.stat(f).st_size, os.path.realpath(f)) for f in glob.glob('*.py')]
-    print(sizes)
+    print("[(os.stat(f).st_size, os.path.realpath(f)) for f in glob.glob('*.py')]={}".format(sizes))
 
+
+def queries():
+    stocks = [
+        {'price': 91.10, 'name': 'IBM', 'shares': 50},
+        {'price': 83.44, 'name': 'CAT', 'shares': 150},
+        {'price': 51.23, 'name': 'MSFT', 'shares': 200},
+        {'price': 40.37, 'name': 'GE', 'shares': 95},
+        {'price': 65.10, 'name': 'MSFT', 'shares': 50},
+        {'price': 70.44, 'name': 'IBM', 'shares': 100}
+    ]
+    print("stocks={}".format(stocks))
+
+    stocknames = [s['name'] for s in stocks]
+    print("[s['name'] for s in stocks]={}".format(stocknames))
+
+    # You can perform database-like queries on sequences
+    query = [s for s in stocks if s['price'] > 50.0 and s['shares'] > 100]
+    print("[s for s in stocks if s['price'] > 50.0 and s['shares'] > 100]={}".format(query))
+
+    query = [s for s in stocks if s['name'] in ('MSFT', 'IBM')]
+    print("[s for s in stocks if s['name'] in ('MSFT', 'IBM')]={}".format(query))
+
+    # combine a list comprehension with a sequence reduction
+    cost = sum([s['shares'] * s['price'] for s in stocks])
+    print("sum([s['shares'] * s['price'] for s in stocks])={}".format(cost))
+
+
+if __name__ == '__main__':
+    """
+    Choose one of examples
+    """
+    function = sys.argv[1]
+    try:
+        locals()[function]()
+    except KeyError as _:
+        print("Choose one of functions to call: create, files, queries")
